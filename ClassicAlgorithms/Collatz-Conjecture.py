2c05f2ea6ca2ffbab724b14225b11aed58acdb64
Zach <zachtmartin@gmail.com>
2017-01-21 12:44:17 -0800

    Collatz Conjecture Algorithm
    It works

---
+++ ClassicAlgorithms/Collatz-Conjecture.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 21 12:05:34 2017
+Count number of steps to 1 using Collatz Conjecture
+@author: zach
+"""
+
+n = input("What's the number?")
+counts = int(0)
+while n > 1:
+    if (n % 2 == 0):
+        n = n / 2
+    else:
+        n = n * 3 + 1
+    counts += 1
+
+print"It took" , counts , "iterations to get to 1."
